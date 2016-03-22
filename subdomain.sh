133fb561f6885e30ce02c5aa75953bb400f4c261
satriaajiputra <satriatamvan@gmail.com>
2016-03-23 05:32:47 +0700

    Update subdomain.sh

--- subdomain.sh
+++ subdomain.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-echo -n "Pertama-tama masukan nama subdomain, misalkan project.idweb.id"
-echo -n "Masukan subdomain 	:"
-read sudomain
+echo -e "Pertama-tama masukan nama subdomain, misalkan project.idweb.id"
+read -p "Masukan subdomain: " subdomain
 
-echo -n "..."
-echo -n "Sedang membuat direktori"
-mkdir /home/$subdomain/public_html
-mkdir /home/$subdomain/logs
+echo -e "..."
+echo -e "Sedang membuat direktori"
+cd /home
+mkdir $subdomain
+mkdir $subdomain/public_html
+mkdir $subdomain/logs
 
-echo -n "Sedang mengatur configurasi NGINX"
-echo -n "..."
+echo -e "Sedang mengatur configurasi NGINX"
+echo -e "..."
 
-wget -O /etc/nginx/conf.d/$subdomain.conf
+wget -O /etc/nginx/conf.d/$subdomain.conf "https://raw.githubusercontent.com/satriaajiputra/debian7os/master/confnginx.conf"
 sed -i 's/subdomaindisini/$subdomain/g' /etc/nginx/conf.d/$subdomain.conf
 
-echo -n "Pembuatan konfigurasi dan folder telah selesai"
-echo -n "Sistem akan melakukan restart pada NGINX dan PHP"
-echo -n "restarting ..."
+echo -e "Pembuatan konfigurasi dan folder telah selesai"
+echo -e "Sistem akan melakukan restart pada NGINX dan PHP"
+echo -e "restarting ..."
 
 service nginx restart
 service php-fpm restart
